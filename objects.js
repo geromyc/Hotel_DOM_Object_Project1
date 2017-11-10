3fafb2ce4de6d39da276ac72044534bae51a5c5c
Geromy Cunningham <geromycunningham@gmail.com>
2017-11-10 01:15:33 +0000

    Initial Commit

---
+++ objects.js
@@ -0,0 +1,17 @@
+var hotel = {
+  name: 'Biltmore',
+  rooms: 500,
+  roomsBooked: 150,
+  roomService: true,
+  pool: false, 
+  
+  roomsAvail: function() {
+    return this.rooms - this.roomsBooked;
+  }
+};
+
+  function tahDah() {
+    document.getElementById("name").innerHTML = hotel.name;
+    document.getElementById("rooms").innerHTML = hotel.rooms;
+    document.getElementById("roomsAvail").innerHTML = hotel.roomsAvail();
+  }
